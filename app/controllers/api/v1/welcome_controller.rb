097bc58afe4a23644292f0027ede579745d264c7
Malisa Middlebrooks <middlebrooks314@gmail.com>
2021-05-04 09:39:14 -0500

    add emphasis to welcome string

--- app/controllers/api/v1/welcome_controller.rb
+++ app/controllers/api/v1/welcome_controller.rb
@@ -2,7 +2,7 @@
 
 class Api::V1::WelcomeController < ApplicationController
   def index
-    output = 'Welcome to the Gilded Rose LLC store!'
+    output = 'Welcome to the Gilded Rose LLC store!!!!!!'
     render json: output
   end
 end
