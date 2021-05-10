e567900ede8be8956e72a61872595c3c87081302
Malisa Middlebrooks <middlebrooks314@gmail.com>
2021-05-10 08:58:19 -0500

    Change JSON format

--- app/controllers/api/v1/welcome_controller.rb
+++ app/controllers/api/v1/welcome_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::WelcomeController < ApplicationController
   def index
-    output = 'Welcome to the Gilded Rose LLC store!!!!!!'
+    output = "Welcome to the Gilded Rose LLC store!!!!!!"
     render json: output
   end
 end
