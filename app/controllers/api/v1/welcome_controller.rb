c83f67ddd344b9320713e9424b7890fc5b588817
Malisa Middlebrooks <middlebrooks314@gmail.com>
2021-05-12 18:15:10 -0500

    Subtract exclamations

--- app/controllers/api/v1/welcome_controller.rb
+++ app/controllers/api/v1/welcome_controller.rb
@@ -1,7 +1,7 @@
 require 'json'
 class Api::V1::WelcomeController < ApplicationController
   def index
-    output = "Welcome to the Gilded Rose LLC store!!!!!!"
+    output = "Welcome to the Gilded Rose LLC store!!"
     render json: output.to_json
   end
 end
