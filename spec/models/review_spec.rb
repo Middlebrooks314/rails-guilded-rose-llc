d6766c17415e12a78947f26134af5463ed7cb193
solidlikeplato <ethanwhaley@gmail.com>
2021-07-19 09:53:16 -0500

    refactored review spec adding before each block

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,26 +1,28 @@
 require "rails_helper"
 
 RSpec.describe Review, type: :model do
-  subject do
-    bacon = Item.new(
+  before(:each) do
+    @bacon = Item.new(
       name: "bacon",
       sell_in: 4,
       quality: 10,
       description: "meat"
     )
-
-    described_class.new(
-      item: bacon,
-      text: "delicious meat"
-    )
   end
 
   it "is valid with valid attributes" do
+    subject = described_class.new(
+      item: @bacon,
+      text: "delicious meat"
+    )
     expect(subject).to be_valid
   end
 
   it "is not valid without text" do
-    subject.text = nil
+    subject = described_class.new(
+      item: @bacon,
+      text: nil
+    )
     expect(subject).to_not be_valid
   end
 end
